3d55a3e84cb2f9a2fbae07ee9a755fb3920182e1
Elsammit <hhashimo25@gmail.com>
2021-05-16 19:39:28 +0900

    コメント追加

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
+#動画表示html
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -30,12 +31,13 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
+#動画データ送信
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+#動画開始
 @app.route('/start')
 def start_movie():
     print("start")
@@ -44,12 +46,14 @@ def start_movie():
     Camera.speed = 0
     return Response("OK", 200)
 
+#動画一時停止
 @app.route('/pause', methods=["POST"])
 def pause_movie():
     print("pause")
     Camera.stop = True
     return Response("OK", 200)
 
+#動画停止
 @app.route('/stop', methods=["POST"])
 def stop_movie():
     print("stop")
@@ -58,6 +62,7 @@ def stop_movie():
     video_feed()
     #return Response("OK", 200)
 
+#早送りスピード調整
 @app.route('/speed', methods=["POST"])
 def changeSpeed():
     if Camera.rewindFlg == True:
@@ -69,6 +74,7 @@ def changeSpeed():
     print("speed:"+str(Camera.speed))
     return Response("OK", 200)
 
+#巻き戻し
 @app.route('/rewind', methods=["POST"])
 def rewind_movie():
     if Camera.rewindFlg == False:
@@ -80,6 +86,7 @@ def rewind_movie():
     print("rewind:"+str(Camera.rewindFlg))
     return Response("OK", 200)
 
+#動画進捗状況送信
 @app.route('/progress', methods=["POST"])
 def get_progress():
     return Response(str(Camera.progress), 200)
